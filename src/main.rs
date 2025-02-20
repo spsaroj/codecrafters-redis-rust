fd2e555e66f42d586c7b4aebf7fa5b37d2782b1a
Saroz <31454493+spsaroj@users.noreply.github.com>
2025-02-19 18:15:00 -0600

    ping pong completion

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,14 @@ fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
+    Uncomment this block to pass the first stage
     
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                stream.write_all(b"+PONG\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
